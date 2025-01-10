cfc666be3e58a6d648dada319805643d26456f6c
Егор Коликов <Egor882002@gmail.com>
2025-01-10 22:29:27 +0300

    CoinM Interfaces added

--- Interfaces/CoinMInterfaces/IPhemexRestClientCoinMApiAccount.cs
+++ Interfaces/CoinMInterfaces/IPhemexRestClientCoinMApiAccount.cs
@@ -1,6 +1,12 @@
-﻿namespace PhemexClient.Interfaces.CoinMInterfaces
+﻿using CryptoExchange.Net.Objects;
+using PhemexClient.Models;
+using PhemexRestClient.Models;
+using System.Security.Principal;
+
+namespace PhemexClient.Interfaces.CoinMInterfaces
 {
     public interface IPhemexRestClientCoinMApiAccount
     {
+        public Task<WebCallResult<PhemexAccountAndPositions>> GetTradingAccountAndPositionsAsync(string currency,CancellationToken cancellationToken=default);
     }
 }
--- Interfaces/CoinMInterfaces/IPhemexRestClientCoinMApiExchangeData.cs
+++ Interfaces/CoinMInterfaces/IPhemexRestClientCoinMApiExchangeData.cs
@@ -1,6 +1,22 @@
-﻿namespace PhemexClient.Interfaces.CoinMInterfaces
+﻿using CryptoExchange.Net.Clients;
+using CryptoExchange.Net.Objects;
+using PhemexClient.Models;
+using PhemexRestClient.Api.Base;
+using PhemexRestClient.Api.SpotApi;
+using PhemexRestClient.Models;
+
+namespace PhemexClient.Interfaces.CoinMInterfaces
 {
     public interface IPhemexRestClientCoinMApiExchangeData
     {
+        public Task<WebCallResult<PhemexRecentTrade>> GetRecentTradesAsync(string symbol, CancellationToken cancellationToken = default);
+        public Task<WebCallResult<IEnumerable<PhemexCoinMTicker>>> GetTickersAsync(CancellationToken cancellationToken = default);
+        public Task<WebCallResult<PhemexCoinMTicker>> GetTickersAsync(string symbol, CancellationToken cancellationToken = default);
+
+        public Task<IEnumerable<ProductBase>> GetSymbolsAsync(CancellationToken cancellationToken = default);
+
+        public Task<WebCallResult<PhemexOrderBook>> GetOrderBookAsync(string symbol, CancellationToken cancellationToken = default);
     }
+
+    
 }
