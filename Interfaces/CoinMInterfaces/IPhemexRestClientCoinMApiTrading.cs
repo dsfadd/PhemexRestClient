5dd2a399fe9fb8d74a75b07047d5fcdbb7bf1f62
Егор Коликов <Egor882002@gmail.com>
2025-01-11 15:11:02 +0300

    PlaceOrderAsync add in IPhemexRestClientCoinMApiTrading

--- Interfaces/CoinMInterfaces/IPhemexRestClientCoinMApiTrading.cs
+++ Interfaces/CoinMInterfaces/IPhemexRestClientCoinMApiTrading.cs
@@ -1,6 +1,31 @@
-﻿namespace PhemexClient.Interfaces.CoinMInterfaces
+﻿using CryptoExchange.Net.CommonObjects;
+using CryptoExchange.Net.Objects;
+using PhemexRestClient.Enums;
+using PhemexRestClient.Models;
+
+namespace PhemexClient.Interfaces.CoinMInterfaces
 {
     public interface IPhemexRestClientCoinMApiTrading
     {
+        public Task<WebCallResult<PhemexCoinMOrder>> PlaceOrderAsync(
+            string symbol,
+            string clOrdID,
+            OrderSide side,
+            long orderQty,
+            long? priceEp = null,
+            OrderType? ordType = null,
+            long? stopPxEp = null,
+            TimeInForceType? timeInForce = null,
+            bool? reduceOnly = null,
+            bool? closeOnTrigger = null,
+            TriggerType? triggerType = null,
+            long? takeProfitEp = null,
+            long? stopLossEp = null,
+            TriggerType? slTrigger = null,
+            TriggerType? tpTrigger = null,
+            long? pegOffsetValueEp = null,
+        TrailingOrderPriceType? pegPriceType = null,
+        CancellationToken cancellationToken = default
+            );
     }
 }
